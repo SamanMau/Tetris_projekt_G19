34ccc40e4b6ef4336356565ee55212158893347c
khanhhuy123 <khanhhuy7591@gmail.com>
2024-03-25 00:44:16 +0100

    UpdateGuiWestPanel
    Added text for point and level to WestPanel

--- Tetris/src/view/LPanel.java
+++ Tetris/src/view/LPanel.java
@@ -1,18 +1,29 @@
 package view;
 
 import javax.swing.*;
+import javax.swing.border.*;
 import java.awt.*;
 
 public class LPanel extends JPanel {
-
+    JLabel poangText = new JLabel();
+    JLabel levelText = new JLabel();
     public LPanel(){
         this.setPreferredSize(new Dimension(150, 300));
         this.setBackground(Color.gray);
-        this.setLayout(null);
+        createPointCounter(0);
+        createLevelCounter(1);
         this.setVisible(true);
     }
 
-    public void createPointCounter(){
-
+    public void createPointCounter(int point){
+        poangText.setText("Poäng: " + point);
+        poangText.setFont(new Font("Calibri", Font.BOLD, 20));
+        this.add(poangText);
+    }
+    public void createLevelCounter(int level){
+        levelText.setText("Level: " + level);
+        levelText.setFont(new Font("Calibri", Font.BOLD, 20));
+        this.add(Box.createVerticalStrut(50));
+        this.add(levelText);
     }
 }
