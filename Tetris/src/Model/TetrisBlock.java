904502d7a653bd544a3d9d08e40865e2ac531087
kingabdull02 <161301313+kingabdull02@users.noreply.github.com>
2024-03-26 12:15:13 +0100

    skapat tetris block

--- Tetris/src/Model/TetrisBlock.java
+++ Tetris/src/Model/TetrisBlock.java
@@ -1,8 +1,45 @@
 package Model;
+import java.util.*;
 
-import java.awt.*;
+public class TetrisBlock  {
+   private int[][] tBlock;
+    private int[][] lBlock;
+    private int[][] zBlock;
+    private int[][] sBlock;
+    private int[][] iBlock;
+    private int[][] oBlock;
+    private int[][] jBlock;
 
-public class TetrisBlock extends Rectangle {
-    public int x, y;
 
+
+   private ArrayList<int[][]> listOfBlocks;
+   public TetrisBlock(){
+       createBlock();
+       listOfBlocks = new ArrayList<>();
+       addBlockToList();
+   }
+
+   public void createBlock(){
+       tBlock = new int[][]{{0, 1, 0}, {1, 1, 1}};
+       lBlock = new int[][]{{1, 0}, {1, 0}, {1, 1}};
+       zBlock = new int[][]{{1,1,0},{0,1,1}};
+       sBlock = new int[][]{{0,1,1},{1,1,0}};
+       iBlock = new int[][]{{1},{1},{1},{1}};
+       jBlock = new int[][]{{0,1},{0,1},{1,1}};
+       oBlock = new int[][]{{1,1},{1,1}};
+   }
+
+   public void addBlockToList(){
+      listOfBlocks.add(tBlock);
+      listOfBlocks.add(lBlock);
+      listOfBlocks.add(zBlock);
+      listOfBlocks.add(sBlock);
+      listOfBlocks.add(iBlock);
+      listOfBlocks.add(oBlock);
+      listOfBlocks.add(jBlock);
+   }
+
+   public ArrayList<int[][]> getBlockList(){
+      return listOfBlocks;
+   }
 }
--- Tetris/src/view/Spelplan.java
+++ Tetris/src/view/Spelplan.java
@@ -1,20 +1,39 @@
 package view;
 
+import Model.TetrisBlock;
+
 import javax.swing.*;
 import java.awt.*;
+import java.util.*;
 
 
-public class Spelplan extends JPanel {
+public class Spelplan extends JPanel{
     private final int column = 10;
     private final int row = 20; // 600 / kvadrat
     private final int kvadrat = 600 / row; //30
+    TetrisBlock tetrisBlock;
+    private ArrayList<int[][]> listOfBlocks; //For testing , should be in controller
 
     public Spelplan(){
         this.setPreferredSize(new Dimension(300, 600));
         this.setBackground(Color.BLACK);
+        tetrisBlock = new TetrisBlock(); //For testing , should be in controller
+        this.listOfBlocks = tetrisBlock.getBlockList(); //For testing , should be in controller
         this.setLayout(null);
         this.setVisible(true);
     }
+
+    private void drawBlock(Graphics g) {
+        for(int row = 0; row < listOfBlocks.get(2).length; row++){
+            for (int col = 0; col < listOfBlocks.get(2)[0].length; col++){
+                if(listOfBlocks.get(2)[row][col] == 1){
+                    g.setColor(Color.red);
+                    g.fillRect(col * kvadrat, row * kvadrat, kvadrat, kvadrat);
+                }
+            }
+        }
+    }
+
 
     @Override
     protected void paintComponent(Graphics g){ //I den här metoden skapar vi rutnät
@@ -37,5 +56,6 @@ public class Spelplan extends JPanel {
                 g.drawRect(width * kvadrat, height * kvadrat, kvadrat, kvadrat);
             }
         }
+        drawBlock(g);
     }
 }
