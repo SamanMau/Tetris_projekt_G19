f71e31453335a3413c8ba8090c22a55ae005ff6b
khanhhuy123 <khanhhuy7591@gmail.com>
2024-04-01 22:37:07 +0200

    Multiple tetris block stops at the end

--- Tetris/src/Model/ListOfBlocks.java
+++ Tetris/src/Model/ListOfBlocks.java
@@ -3,29 +3,41 @@ import java.awt.*;
 import java.util.*;
 
 public class ListOfBlocks  {
-    private TetrisBlock tBlock;
-    private TetrisBlock lBlock;
-    private TetrisBlock zBlock;
-    private TetrisBlock sBlock;
-    private TetrisBlock iBlock;
-    private TetrisBlock oBlock;
-    private TetrisBlock jBlock;
-    private ArrayList<TetrisBlock> listOfBlocks;
+    private int[][] tBlock;
+    private int[][] lBlock;
+    private int[][] zBlock;
+    private int[][] sBlock;
+    private int[][] iBlock;
+    private int[][] oBlock;
+    private int[][] jBlock;
+    private ArrayList<int[][]> listOfBlocks;
+    private ArrayList<Color> listOfColors;
+
 
     public ListOfBlocks(){
         createBlock();
         listOfBlocks = new ArrayList<>();
+        this.listOfColors = new ArrayList<>();
         addBlockToList();
+        addColorsToList();
     }
 
     public void createBlock(){
-        tBlock = new TetrisBlock(new int[][]{{0, 1, 0}, {1, 1, 1}}, Color.white);
+        /*tBlock = new TetrisBlock(new int[][]{{0, 1, 0}, {1, 1, 1}}, Color.white);
         lBlock = new TetrisBlock(new int[][]{{1, 0}, {1, 0}, {1, 1}}, Color.orange);
         zBlock = new TetrisBlock(new int[][]{{1,1,0},{0,1,1}}, Color.green);
         sBlock = new TetrisBlock(new int[][]{{0,1,1},{1,1,0}}, Color.red);
         iBlock = new TetrisBlock(new int[][]{{1},{1},{1},{1}}, Color.cyan);
         jBlock = new TetrisBlock(new int[][]{{0,1},{0,1},{1,1}}, Color.pink);
-        oBlock = new TetrisBlock(new int[][]{{1,1}, {1,1}}, Color.yellow);
+        oBlock = new TetrisBlock(new int[][]{{1,1}, {1,1}}, Color.yellow);*/
+
+        tBlock = new int[][]{{0, 1, 0}, {1, 1, 1}};
+        lBlock = new int[][]{{1, 0}, {1, 0}, {1, 1}};
+        zBlock = new int[][]{{1,1,0},{0,1,1}};
+        sBlock = new int[][]{{0,1,1},{1,1,0}};
+        iBlock = new int[][]{{1},{1},{1},{1}};
+        jBlock = new int[][]{{0,1},{0,1},{1,1}};
+        oBlock = new int[][]{{1,1}, {1,1}};
 
     }
 
@@ -38,8 +50,18 @@ public class ListOfBlocks  {
         listOfBlocks.add(oBlock);
         listOfBlocks.add(jBlock);
     }
-
-    public ArrayList<TetrisBlock> getBlockList(){
+    
+    public void addColorsToList(){
+        listOfColors.add(Color.white);
+        listOfColors.add(Color.orange);
+        listOfColors.add(Color.green);
+        listOfColors.add(Color.red);
+        listOfColors.add(Color.cyan);
+        listOfColors.add(Color.pink);
+        listOfColors.add(Color.yellow);
+    }
+    public ArrayList<int[][]> getBlockList(){
         return listOfBlocks;
     }
+    public ArrayList<Color> getListOfColors(){return listOfColors;}
 }
--- Tetris/src/view/Spelplan.java
+++ Tetris/src/view/Spelplan.java
@@ -15,24 +15,28 @@ public class Spelplan extends JPanel{
     private final int row = 20;
     private final int kvadrat = 600 / row;
     private ListOfBlocks listOfBlocksObj; //For testing , should be in controller
-    private ArrayList<TetrisBlock> listOfBlocks; //For testing , should be in controller
+    private ArrayList<int[][]> listOfShape; //For testing , should be in controller
+    private ArrayList<Color> listOfColors;
     private Timer speed;
     private TetrisBlock block;
     private Random rd = new Random();
     private int randomNum = rd.nextInt(7); // 7
     private boolean collision;
+
+    private Color[][] board = new Color[20][10];
     private boolean gameState = false;
 
     public Spelplan(){
         this.setPreferredSize(new Dimension(300, 600));
         this.setBackground(Color.BLACK);
         listOfBlocksObj = new ListOfBlocks(); //For testing , should be in controller
-        this.listOfBlocks = listOfBlocksObj.getBlockList(); //For testing , should be in controller
+        this.listOfShape = listOfBlocksObj.getBlockList(); //For testing , should be in controller
+        this.listOfColors = listOfBlocksObj.getListOfColors();
         this.setLayout(null);
         this.setVisible(true);
-        block = listOfBlocks.get(randomNum);
         collision = false;
-       // startTimer();
+        generateBlock();
+        startTimer(true);
     }
 
     public void startTimer(boolean gameState){
@@ -42,7 +46,10 @@ public class Spelplan extends JPanel{
                 @Override
                 public void actionPerformed(ActionEvent e) {
                     if(collision){
-                        generateNewBlock();
+                       // generateNewBlock();
+                        addColorToBoard();
+                        generateBlock();
+                        collision = false;
                     }
                     if((block.getShape().length + block.getY()) * kvadrat == 600){
                         collision = true;
@@ -58,6 +65,54 @@ public class Spelplan extends JPanel{
             this.speed.start();
         }
 
+    }
+
+    private void addColorToBoard(){
+        int y = block.getY();
+        int x = block.getX();
+        int[][] shape = block.getShape();
+
+        for(int row = 0; row < block.getShape().length; row++){
+            for (int col = 0; col < block.getShape()[0].length; col++){
+                if(block.getShape()[row][col] == 1){
+                    board[y + row][x + col] = block.getColor();
+                }
+            }
+        }
+    }
+
+    private void generateBlock(){
+        randomNum = rd.nextInt(7);
+        int[][] shape = listOfShape.get(randomNum);
+        Color color = listOfColors.get(randomNum);
+        block = new TetrisBlock(shape, color);
+    }
+
+    @Override
+    protected void paintComponent(Graphics g){ //I den här metoden skapar vi rutnät
+        super.paintComponent(g);
+        /*
+        De två sista argumenten i "drawRect" är "width" och "height". Anledningen till varför
+        vi skickar in kvadrat som argument är eftersom vi har initierat längden på kvadraten
+        lite längre upp. Eftersom en kvadrat är lika långt på både höjden och bredden, så
+        skickar vi in storleken på kvadrat på width och height.
+         */
+        for(int width = 0; width < column; width++){
+            for(int height = 0; height < row; height++){
+                if (board[height][width] != null) {
+                    g.setColor(board[height][width]);
+                    g.fillRect(width * kvadrat, height * kvadrat, kvadrat, kvadrat);
+                    g.setColor(Color.BLACK);
+                    g.drawRect(width * kvadrat, height * kvadrat, kvadrat, kvadrat);
+                }
+                else {
+                    g.setColor(Color.gray);
+                    g.drawRect(width * kvadrat, height * kvadrat, kvadrat, kvadrat);
+                }
+            }
+        }
+
+        drawBlock(g, block);
     }
 
     private void drawBlock(Graphics g, TetrisBlock block) {
@@ -80,28 +135,5 @@ public class Spelplan extends JPanel{
         }
     }
 
-    private void generateNewBlock(){
-        listOfBlocks.get(randomNum).resetCoordinate();
-        randomNum = rd.nextInt(listOfBlocksObj.getBlockList().size());
-        this.block = listOfBlocks.get(randomNum);
-    }
 
-    @Override
-    protected void paintComponent(Graphics g){ //I den här metoden skapar vi rutnät
-        super.paintComponent(g);
-        /*
-        De två sista argumenten i "drawRect" är "width" och "height". Anledningen till varför
-        vi skickar in kvadrat som argument är eftersom vi har initierat längden på kvadraten
-        lite längre upp. Eftersom en kvadrat är lika långt på både höjden och bredden, så
-        skickar vi in storleken på kvadrat på width och height.
-         */
-        for(int width = 0; width < column; width++){
-            for(int height = 0; height < row; height++){
-                g.drawRect(width * kvadrat, height * kvadrat, kvadrat, kvadrat);
-            }
-        }
-
-        drawBlock(g, block);
-
-    }
 }
